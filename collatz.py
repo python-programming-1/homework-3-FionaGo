91eb1d2dc21dc8830c328e2464c56d25d01d6f0e
Fiona Li <fiona.li@ticketmaster.com>
2019-07-17 13:54:00 -0700

    added zero handling

--- collatz.py
+++ collatz.py
@@ -1,45 +1,48 @@
 def collatz():
     """A function to replicate Collatz Conjecture
-       
+
     Parameters
     ----------
     num: integer
-    
+
     Returns
     -------
     Print out all computed results
-    
+
     Raises
     ------
     Raise value error if a non integer was input by the user.
-    
+
     Notes
     -----
     This function will perform the below computations:
-    The coltz conjecture takes a user input integer, 
+    The coltz conjecture takes a user input integer,
         evaluating whether the number is an even or an odd:
         If it's even, coltz will print and return the number //2.
         If it's odd, coltz will print and return 3*num + 1
-        
+
     References
-    ----------    
+    ----------
     https://en.wikipedia.org/wiki/Collatz_conjecture
-    
+
     """
     while True:
-        try: 
-            num = int(input('Hey you, welcome to Collatz! Please enter an integer:'))
+        try:
+            num = int(input('Hey you, I\'m Collatz! Please enter a positive integer:'))
             while num != 1:
-                if num % 2 == 0:
+                if num == 0:
+                    print("Zero is not a positive integer, try again!")
+                    break
+                elif num % 2 == 0:
                     num = num // 2
                 elif num % 2 == 1:
                     num = num * 3 + 1
                 print(num)
-                
+
         except ValueError:
-            print("The input was not a valid integer. Please rerun the program and input an integer!") 
+            print("The input was not a valid integer. Please rerun the program and input a postive integer!")
             break
-            
+
         finally:
             return print("----- That's the end of the program :) ----")
 
